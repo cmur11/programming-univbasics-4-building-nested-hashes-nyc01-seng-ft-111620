0369f853952e5a161a05caf483a69afc7b8fcdf8
Conor Murnane <conormurnane11@gmail.com>
2020-10-30 01:09:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,13 +13,13 @@ def monopoly_with_second_tier
   # complexity.
  
 
-   
+   base_hash = {
     :railroads => {
       :pieces => 4 ,
       :rent_in_dollars => {},
       :names => {}
     }
-   
+   }
  
 end
 
